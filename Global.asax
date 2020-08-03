4978bc3f53151e8d0b32ec928902877cff5e72f3
gps57 <glen@swiftbt.com>
2020-08-03 15:52:12 -0400

    Initial Submit
    Updated to exclude configuration files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AphidBT.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AphidBT.Startup))]
+namespace AphidBT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
